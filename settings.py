908fdd6176da8cc564b413f2eae51441b6055bca
BnoGaming <meghmodi2004@gmail.com>
2025-02-19 19:27:22 +0530

    Added player and movement

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
 import math
 
 # game settings
-RES = WIDTH, HEIGHT = 800, 600
-
+RES = WIDTH, HEIGHT = 1600, 900
 FPS = 0
+
+#player
+PLAYER_POS = 1.5, 5  # mini_map
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
+PLAYER_SIZE_SCALE = 60
+PLAYER_MAX_HEALTH = 100
